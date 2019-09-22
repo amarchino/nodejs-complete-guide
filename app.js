ff28432d79f0f58649066199ab7df45650e3e0db
amarchino <marchino.alessandro@gmail.com>
2019-09-22 14:06:36 +0200

    Lezione 33

--- app.js
+++ app.js
@@ -13,10 +13,17 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
   if(url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
+    const body = [];
+    req.on('data', chunk => body.push(chunk));
+    req.on('end', () => {
+      const parseBody = Buffer.concat(body).toString();
+      const message = parseBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      return res.end();
+    });
+    return;
   }
   res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
