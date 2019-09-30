d41fbd96df82bbdd8868e83b8e394f45cab44810
amarchino <marchino.alessandro@gmail.com>
2019-09-30 22:55:07 +0200

    Lezione 79

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,8 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   console.log(adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop');
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
