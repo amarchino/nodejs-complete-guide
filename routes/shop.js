183813f339d1f726e9a1e7d1f5cf590642771069
amarchino <marchino.alessandro@gmail.com>
2019-10-02 23:03:55 +0200

    Lezione 87

--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,7 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('shop', {prods: adminData.products, pageTitle: 'Shop', path: '/'});
+  res.render('shop', {prods: adminData.products, pageTitle: 'Shop', path: '/', hasProducts: adminData.products.length > 0});
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
