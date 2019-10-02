ddc13b5b964f1c7d31b52d1e69ee3a28a85f0a55
amarchino <marchino.alessandro@gmail.com>
2019-10-03 00:03:32 +0200

    Lezione 91

--- routes/admin.js
+++ routes/admin.js
@@ -1,15 +1,9 @@
-const path = require('path');
 const express = require('express');
-const rootDir = require('../util/path');
-
 const router = express.Router();
 
 const products = [];
 
-router.get('/add-product', (req, res) => {
-  res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'})
-  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
-});
+router.get('/add-product', (req, res) => res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'}));
 
 router.post('/add-product', (req, res) => {
   products.push(req.body);
--- routes/shop.js
+++ routes/shop.js
@@ -1,14 +1,8 @@
-const path = require('path');
 const express = require('express');
-
-const rootDir = require('../util/path');
 const adminData = require('./admin');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('shop', {prods: adminData.products, pageTitle: 'Shop', path: '/'});
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-});
+router.get('/', (req, res) => res.render('shop', {prods: adminData.products, pageTitle: 'Shop', path: '/'}));
 
 module.exports = router;
