f0f008506f572db8a8a77c6b79936cafe381255c
amarchino <marchino.alessandro@gmail.com>
2019-10-04 23:25:56 +0200

    Lezione 97

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
 
 const app = express();
 
@@ -16,10 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res) => {
-  res.status(404)
-    // .sendFile(path.join(__dirname, 'views', '404.html'));
-    .render('404', {pageTitle: 'Page Not Found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
