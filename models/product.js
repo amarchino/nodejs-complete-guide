0017359eb9bf367bf90ada0ee5ef89aaaf803f7f
amarchino <marchino.alessandro@gmail.com>
2019-10-05 22:07:41 +0200

    Lezione 101

--- models/product.js
+++ models/product.js
@@ -2,28 +2,30 @@ const fs = require('fs');
 const path = require('path');
 const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
 
+const getProductsFromFile = cb => fs.readFile(p, (err, fileContent) => {
+  if(err) {
+    return cb([]);
+  }
+  return cb(JSON.parse(fileContent));
+});
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
 
   save() {
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => console.error(err));
+      fs.writeFile(p, JSON.stringify(products), err => {
+        if(err) {
+          console.error(err);
+        }
+      });
     });
   }
 
   static fetchAll(cb) {
-    fs.readFile(p, (err, fileContent) => {
-      if(err) {
-        return cb([]);
-      }
-      return cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
   }
 };
