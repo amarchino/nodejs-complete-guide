4b9b7e7f135cdfb519a07a0a73d9d08a48f6f2ed
amarchino <marchino.alessandro@gmail.com>
2019-10-05 22:36:23 +0200

    Lezione 105

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,9 @@
 const Product = require('../models/product');
 
-exports.getAddProduct = (req, res) => res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
+exports.getAddProduct = (req, res) => res.render('admin/add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
 exports.postAddProduct = (req, res) => {
   const product = new Product(req.body.title);
   product.save();
   res.redirect('/');
 };
-exports.getProducts = (req, res) => Product.fetchAll(prods => res.render('shop', {prods, pageTitle: 'Shop', path: '/'}));
+exports.getProducts = (req, res) => Product.fetchAll(prods => res.render('shop/product-list', {prods, pageTitle: 'Shop', path: '/'}));
